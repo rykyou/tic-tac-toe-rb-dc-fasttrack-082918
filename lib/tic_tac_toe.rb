fa1b9242d5195ada9b8580772441e7c5464dbe76
Robin Kim <rkim1214@gmail.com>
2018-10-06 15:28:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,6 +42,7 @@ def turn(board)
   index = input_to_index(user_input)
   
   if valid_move?(board, index)
+    token = current_player(board)
     move(board, index, token)
     display_board(board)
   else 
