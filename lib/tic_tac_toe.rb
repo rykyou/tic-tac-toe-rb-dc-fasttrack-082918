abf9c55ec946a55d0aa832a1dd28dd663f29cb4a
Robin Kim <rkim1214@gmail.com>
2018-10-06 15:33:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,9 +117,9 @@ def play(board)
     turn(board)
   end 
   
-    if won?(board) 
-      puts "Congratulations #{winner(board)}!"
-    elsif draw?(board) 
-      puts "Cat's Game!"
-    end
+  if won?(board) 
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board) 
+    puts "Cat's Game!"
+  end
 end
